0f830445988e265a6268c5e2e28bb1a87807ce62
denys <denysburimov@gmail.com>
2022-03-30 16:20:43 +0300

    Database Prototypes v01

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,6 @@
+from cgi import print_form
 from datetime import datetime
+from enum import unique
 
 from flask_login import UserMixin, AnonymousUserMixin
 from sqlalchemy import func
@@ -16,8 +18,8 @@ class User(db.Model, UserMixin, ModelMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
-    activated = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
+    accounts = db.relationship("Account", backref="user", lazy=True)
 
     @hybrid_property
     def password(self):
@@ -32,12 +34,36 @@ class User(db.Model, UserMixin, ModelMixin):
         user = cls.query.filter(
             func.lower(cls.username) == func.lower(username)
         ).first()
-        if user is not None and check_password_hash(user.password, password):
+        if user and check_password_hash(user.password, password):
             return user
 
     def __repr__(self):
-        return f"<{self.id}: {self.username}>"
+        return f"<user_id: {self.id}, username: {self.username}>"
 
 
-class AnonymousUser(AnonymousUserMixin):
-    pass
+class Account(db.Model):
+    account_id = db.Column(db.Integer, primary_key=True)
+    reseller = db.Column(db.String(64), db.ForeignKey("user.username"))
+    login = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    
+    def __repr__(self):
+        return f"account_id: {self.account_id}, reseller: {self.reseller}"
+    
+    
+class Billing(db.Model):
+    reseller = db.Column(db.String(64), db.ForeignKey("user.username"))
+    credits = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    
+    def __repr__(self):
+        return f"reseller: {self.reseller}, credits: {self.credits}, created: {self.created_at}"
+    
+
+class History(db.Model):
+    instance = db.Column(db.String(64), nullable=False)
+    created_by = db.Column(db.String(64), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    
+    def __repr__(self):
+        return f"instance: {self.instance}, created_by: {self.created_by}, created_at: {self.created_at}"
