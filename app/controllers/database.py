330e98347761c643cddaea973376187eed03f3dc
denys <denysburimov@gmail.com>
2022-03-31 11:14:59 +0300

    db init updated

--- app/controllers/database.py
+++ app/controllers/database.py
@@ -2,6 +2,8 @@ from flask import current_app as app
 from app.models import User
 from app import db
 from app.logger import log
+from app.models.account import Account
+from app.models.billing import Billing
 
 TEST_USERS_NUMBER = 20
 
@@ -21,5 +23,8 @@ def init_db(add_test_data: bool = False):
         log(log.INFO, "Generate test data")
         for i in range(TEST_USERS_NUMBER):
             User(username=f"user_{i}", password="pass").save(False)
+            Account(user_id=i, login=f"user_{i}_account_01", password="pass").save(False)
+            Account(user_id=i, login=f"user_{i}_account_02", password="pass").save(False)
+            Billing(user_id=i, credits=1000).save(False)
 
     db.session.commit()
--- app/views/main.py
+++ app/views/main.py
@@ -1,5 +1,7 @@
 from flask import render_template, Blueprint
 from app.models import User
+from app.models.account import Account
+from app.models.billing import Billing
 
 main_blueprint = Blueprint("main", __name__)
 
@@ -7,4 +9,6 @@ main_blueprint = Blueprint("main", __name__)
 @main_blueprint.route("/")
 def index():
     users = User.query.all()
-    return render_template("index.html", users=users)
+    accounts = Account.query.all()
+    billings = Billing.query.all()
+    return render_template("index.html", users=users, accounts=accounts, billings=billings)
