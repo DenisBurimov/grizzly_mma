06d6fb95115d15b62b5e19877fc203143d5caac8
denys <denysburimov@gmail.com>
2022-03-31 10:09:19 +0300

    Models before separation, without History and without nested class Role

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,10 @@
-from cgi import print_form
 from datetime import datetime
-from enum import unique
+from email.policy import default
 
 from flask_login import UserMixin, AnonymousUserMixin
 from sqlalchemy import func
 from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db
@@ -13,15 +13,17 @@ from app.models.utils import ModelMixin
 
 class User(db.Model, UserMixin, ModelMixin):
 
-    __tablename__ = "user"
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
-    role = db.Column(db.String(64))
+    role = db.Column(db.String(64))  # TODO: Enum
     created_at = db.Column(db.DateTime, default=datetime.now)
-    accounts = db.relationship("Account", backref="user", lazy=True)
-    billings = db.relationship("Billing", backref="user", lazy=True)
+    deleted = db.Column(db.Boolean, default=False)
+    
+    accounts = relationship("Account", viewonly=True)
+    billings = relationship("Billing", viewonly=True)
 
     @hybrid_property
     def password(self):
@@ -40,34 +42,44 @@ class User(db.Model, UserMixin, ModelMixin):
             return user
 
     def __repr__(self):
-        return f"<user_id: {self.id}, username: {self.username}>"
+        return f"<{self.id}: {self.username}>"
 
+# TODO: install and configure
+# TODO: Each class in separate file
 
-class Account(db.Model):
-    account_id = db.Column(db.Integer, primary_key=True)
-    reseller = db.Column(db.Integer, db.ForeignKey("user.id"))
-    login = db.Column(db.String(64), unique=True, nullable=False)
-    password = db.Column(db.String(64), nullable=False)
+class Account(db.Model, ModelMixin):
+    
+    __tablename__ = "accounts"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    login = db.Column(db.String(16), unique=True, nullable=False)
+    password = db.Column(db.String(16), nullable=False)
     
     def __repr__(self):
-        return f"account_id: {self.account_id}, reseller: {self.reseller}"
+        return f"<{self.account_id}. {self.reseller}"
     
     
-class Billing(db.Model):
-    reseller_id = db.Column(db.Integer, primary_key=True)
-    reseller = db.Column(db.Integer, db.ForeignKey("user.id"))
+class Billing(db.Model, ModelMixin):
+    
+    __tablename__ = "billings"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     credits = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, default=datetime.now)
     
+    reseller = relationship("User")
+    
     def __repr__(self):
-        return f"reseller: {self.reseller}, credits: {self.credits}, created: {self.created_at}"
+        return f"<{self.id}, reseller: {self.reseller}, credits: {self.credits}, created: {self.created_at}"
     
 
-class History(db.Model):
-    operation_id = db.Column(db.Integer, primary_key=True)
-    instance = db.Column(db.String(64), nullable=False)
-    created_by = db.Column(db.String(64), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.now)
+# class History(db.Model, ModelMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     instance = db.Column(db.String(64), nullable=False)
+#     created_by = db.Column(db.String(64), nullable=False)
+#     created_at = db.Column(db.DateTime, default=datetime.now)
     
-    def __repr__(self):
-        return f"instance: {self.instance}, created_by: {self.created_by}, created_at: {self.created_at}"
+#     def __repr__(self):
+#         return f"<{self.id}, instance: {self.instance}, created_by: {self.created_by}, created_at: {self.created_at}"
--- app/views/main.py
+++ app/views/main.py
@@ -1,8 +1,10 @@
 from flask import render_template, Blueprint
+from app.models import User
 
 main_blueprint = Blueprint("main", __name__)
 
 
 @main_blueprint.route("/")
 def index():
-    return render_template("index.html")
+    users = User.query.all()
+    return render_template("index.html", users=users)
