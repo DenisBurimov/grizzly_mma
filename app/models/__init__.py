7aa8caf4507c4fd52e15fe16a9646f539af04860
denys <denysburimov@gmail.com>
2022-03-31 10:31:28 +0300

    Models moved to separate files. History table added, but not imported to the upper level

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,4 @@
 # flake8: noqa F401
 from .user import User
+from .account import Account
+from .billing import Billing
---
+++ app/models/billing.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from sqlalchemy.orm import relationship
+from app import db
+from app.models.utils import ModelMixin
+
+
+class Billing(db.Model, ModelMixin):
+    
+    __tablename__ = "billings"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    credits = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    
+    reseller = relationship("User")
+    
+    def __repr__(self):
+        return f"<{self.id}, reseller: {self.reseller}, credits: {self.credits}, created: {self.created_at}"
+    
