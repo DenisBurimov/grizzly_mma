86fe42204e203b99510d743d504b5da84eeaa05e
denys <denysburimov@gmail.com>
2022-04-01 08:47:41 +0300

    Accounts page

--- app/views/accounts.py
+++ app/views/accounts.py
@@ -1,10 +1,14 @@
 from flask import render_template, Blueprint
-from app.models.account import Account
+from flask_login import current_user
+from app.models import Account, User
 
 accounts_blueprint = Blueprint("accounts", __name__)
 
 
 @accounts_blueprint.route("/accounts")
 def accounts_page():
-    accounts = Account.query.all()
+    if current_user != User.Role.admin:
+        accounts = Account.query.filter_by(user_id=current_user.id)
+    else:
+        accounts = Account.query.all()
     return render_template("accounts.html", accounts=accounts)
