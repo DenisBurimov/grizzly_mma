86fb20d3fb4d9c3161e610a9a2a0e4447b95059d
denys <denysburimov@gmail.com>
2022-04-05 17:40:51 +0300

    JS validation correction

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -9,7 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
         code = str.charCodeAt(i);
         if (!(code > 47 && code < 58) && // numeric (0-9)
             !(code > 64 && code < 91) && // upper alpha (A-Z)
-            !(code > 96 && code < 123)) { // lower alpha (a-z)
+            !(code > 96 && code < 123) &&// lower alpha (a-z)
+            !(code == 45) && // hyphen
+            !(code == 95)) { // underscore
           return false;
         }
       }
