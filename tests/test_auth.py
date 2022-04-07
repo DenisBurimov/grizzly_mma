1d78278979a523c4f28c3876d1b7585ca3c6a12c
denys <denysburimov@gmail.com>
2022-04-07 11:49:05 +0300

    work

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -31,15 +31,14 @@ def test_login_and_logout(client):
     # Access to logout view before login should fail.
     response = logout(client)
     assert b"Please log in to access this page." in response.data
-    register("sam")
-    response = login(client, "sam")
-    assert b"You are successfuly logged in!" in response.data
+    response = login(client, "admin", "admin")
+    assert b"Users" in response.data
     # Should successfully logout the currently logged in user.
-    # response = logout(client)
-    # assert b"You were logged out." in response.data
+    response = logout(client)
+    assert b"Please log in to access this page." in response.data
     # Incorrect login credentials should fail.
-    response = login(client, "sam", "wrongpassword")
+    response = login(client, "x", "y")
     assert b"Wrong username or password." in response.data
     # Correct credentials should login
-    response = login(client, "sam")
+    response = login(client, "admin", "admin")
     assert b"You are successfuly logged in!" in response.data
--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,7 @@
+import time
+import timecop
+from flask import url_for
+from onetimepass import get_hotp, get_totp
 from app.models import User
 
 from app.controllers.database import TEST_PASS
@@ -9,10 +13,33 @@ def register(username, password=TEST_PASS):
     return user.id
 
 
-def login(client, username, password=TEST_PASS):
-    return client.post(
-        "/login", data=dict(username=username, password=password), follow_redirects=True
-    )
+def login(client, user_name, password="password"):
+    secret = b"MFRGGZDFMZTWQ2LK"
+    with timecop.freeze(time.time()):
+        user = User.query.filter(User.username == user_name).first()
+        assert user
+        user.otp_secret = secret
+        user.otp_active = True
+        user.save()
+        hotp = get_hotp(
+            secret=secret,
+            intervals_no=int(time.time()) // 30,
+        )
+        totp = get_totp(secret=secret)
+        assert hotp == totp
+        res = client.post(
+            "/login",
+            data=dict(user_name=user_name, password=password),
+            follow_redirects=True,
+        )
+        assert res.status_code == 200
+        if b"Wrong user ID or password." in res.data:
+            return res
+        return client.post(
+            url_for("auth.otp_verify"),
+            data=dict(token=f"{totp:06d}"),
+            follow_redirects=True,
+        )
 
 
 def logout(client):
