a7fb173ab2b7449b7d9223f0a2dd76b949aad048
denys <denysburimov@gmail.com>
2022-05-25 09:19:36 +0300

    Multisearch unions finance

--- app/views/finance.py
+++ app/views/finance.py
@@ -44,29 +44,35 @@ def finance_search(query):
         flash("Access denied", "danger")
         return redirect(url_for("main.index"))
     page = request.args.get("page", 1, type=int)
-
-    if query.isdigit():
-        finance_data = Transaction.query.filter(Transaction.transaction_amount == query)
-
-    if finance_data.count() == 0 or not query.isdigit():
-        try:
-            # Dates
-            search_date = datetime.datetime.strptime(query, "%Y-%m-%d")
-            next_day = search_date + datetime.timedelta(1)
-            finance_data = finance_data.filter(
-                search_date <= Transaction.created_at,
-                Transaction.created_at <= next_day,
-            )
-        except Exception:
-            users = User.query.filter(User.username.like(f"%{query}%")).all()
-            users_id_list = [user.id for user in users]
+    splitted_queries = query.split(",")
+    search_result = Transaction.query.filter(Transaction.id == 0)
+    for raw_single_query in splitted_queries:
+        single_query = raw_single_query.strip()
+        if single_query.isdigit():
             finance_data = Transaction.query.filter(
-                (Transaction.reseller_id.in_(users_id_list))
-                | (Transaction.admin_id.in_(users_id_list))
-                | (Transaction.comment.like(f"%{query}%"))
+                Transaction.transaction_amount == int(single_query)
             )
 
-    finance_data = finance_data.paginate(
+        if finance_data.count() == 0 or not single_query.isdigit():
+            try:
+                # Dates
+                search_date = datetime.datetime.strptime(single_query, "%Y-%m-%d")
+                next_day = search_date + datetime.timedelta(1)
+                finance_data = finance_data.filter(
+                    search_date <= Transaction.created_at,
+                    Transaction.created_at <= next_day,
+                )
+            except Exception:
+                users = User.query.filter(User.username.like(f"%{single_query}%")).all()
+                users_id_list = [user.id for user in users]
+                finance_data = Transaction.query.filter(
+                    (Transaction.reseller_id.in_(users_id_list))
+                    | (Transaction.admin_id.in_(users_id_list))
+                    | (Transaction.comment.like(f"%{single_query}%"))
+                )
+        search_result = search_result.union(finance_data)
+
+    finance_data = search_result.paginate(
         page=page, per_page=current_app.config["PAGE_SIZE"]
     )
 
